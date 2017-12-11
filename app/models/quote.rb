37e367f279763b3a19ed7e0ce63d7a207d4b0941
Mark Helt <heltmm@gmail.com>
2017-12-10 17:50:03 -0800

    specs, search scope

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ApplicationRecord
   validates :author, presence: true
   validates :content, presence: true
+
+  scope :search, -> (author_param) { where("author like ?", "%#{author_param}")}
 end
